9c40152856015d02d484d47e5bb5444fde706bf2
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2021-01-13 14:57:03 +0100

    Fjerna rest-api

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/Application.kt
@@ -1,25 +1,16 @@
 package no.nav.hjelpemidler.soknad.mottak
 
-import io.ktor.application.Application
-import io.ktor.application.install
-import io.ktor.features.CallLogging
-import io.ktor.request.path
 import no.nav.helse.rapids_rivers.RapidApplication
 import no.nav.helse.rapids_rivers.RapidsConnection
-import no.nav.hjelpemidler.soknad.mottak.db.SoknadStore
 import no.nav.hjelpemidler.soknad.mottak.db.SoknadStorePostgres
 import no.nav.hjelpemidler.soknad.mottak.db.dataSourceFrom
 import no.nav.hjelpemidler.soknad.mottak.db.migrate
 import no.nav.hjelpemidler.soknad.mottak.service.SoknadDataSink
-import org.slf4j.event.Level
 
 fun main() {
     val store = SoknadStorePostgres(dataSourceFrom(Configuration))
 
     RapidApplication.Builder(RapidApplication.RapidApplicationConfig.fromEnv(Configuration.rapidApplication))
-        .withKtorModule {
-            api(store)
-        }
         .build().apply {
             SoknadDataSink(this, store)
         }.apply {
@@ -33,10 +24,3 @@ fun main() {
         }.start()
 }
 
-internal fun Application.api(store: SoknadStore) {
-
-    install(CallLogging) {
-        level = Level.INFO
-        filter { it.request.path().startsWith("/api") }
-    }
-}
