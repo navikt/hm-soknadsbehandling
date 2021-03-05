af8de4a93cdc2b3ba3c20fd373adeb37489737dc
matsbyfl <mats@byfuglien.net>
2021-03-05 12:30:07 +0100

    Henter ut formidlers navn i søknadsoversikt

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/service/SoknadMedStatus.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/service/SoknadMedStatus.kt
@@ -1,10 +1,20 @@
 package no.nav.hjelpemidler.soknad.mottak.service
 
+import com.fasterxml.jackson.databind.JsonNode
 import java.util.Date
 import java.util.UUID
 
 class SoknadMedStatus(
     val soknadId: UUID,
     val datoOpprettet: Date,
-    val status: Status
-)
+    val datoOppdatert: Date,
+    val status: Status,
+    soknad: JsonNode,
+) {
+    val formidlerNavn = formidlerNavn(soknad)
+}
+
+private fun formidlerNavn(soknad: JsonNode): String {
+    val leveringNode = soknad["soknad"]["levering"]
+    return "${leveringNode["hmfFornavn"].textValue()} ${leveringNode["hmfEtternavn"].textValue()}"
+}
