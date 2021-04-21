5019dfe6e4fce36108c66b043bf4f82d22ce2387
Gustav Lillesæter <gustav.lillesater@nav.no>
2021-04-21 15:43:19 +0200

    legg til eventId i melding til rapid
    Co-authored-by: avoldsund <anders.voldsund@nav.no>

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/service/VedtaksresultatLagretData.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/service/VedtaksresultatLagretData.kt
@@ -1,5 +1,6 @@
 package no.nav.hjelpemidler.soknad.mottak.service
 
+import com.github.guepardoapps.kulid.ULID
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageProblems
 import java.util.UUID
@@ -12,6 +13,7 @@ internal data class VedtaksresultatLagretData(
     internal fun toJson(eventName: String): String {
         return JsonMessage("{}", MessageProblems("")).also {
             it["eventName"] = eventName
+            it["eventId"] = ULID.random()
             it["søknadId"] = this.søknadId
             it["fnrBruker"] = this.fnrBruker
             it["vedtaksresultat"] = this.vedtaksresultat
