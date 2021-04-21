4f4af79f783fb0ee659826ea4106f942a2d24e3c
Gustav Lillesæter <gustav.lillesater@nav.no>
2021-04-21 15:02:58 +0200

    legg til vedtaksresultat i melding til rapid
    Co-authored-by: avoldsund <anders.voldsund@nav.no>

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/service/VedtaksresultatLagretData.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/service/VedtaksresultatLagretData.kt
@@ -7,13 +7,14 @@ import java.util.UUID
 internal data class VedtaksresultatLagretData(
     val søknadId: UUID,
     val fnrBruker: String,
-    val vedtaksresultat: String? = null,
+    val vedtaksresultat: String,
 ) {
     internal fun toJson(eventName: String): String {
         return JsonMessage("{}", MessageProblems("")).also {
             it["eventName"] = eventName
             it["søknadId"] = this.søknadId
             it["fnrBruker"] = this.fnrBruker
+            it["vedtaksresultat"] = this.vedtaksresultat
         }.toJson()
     }
 }
