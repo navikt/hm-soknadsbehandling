4b07cd3309cd102d279a52a602a28e62666eea56
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2021-04-22 10:34:52 +0200

    fix etter merge med main

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/river/VedtaksresultatFraInfotrygd.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/river/VedtaksresultatFraInfotrygd.kt
@@ -11,6 +11,9 @@ import no.nav.helse.rapids_rivers.River
 import no.nav.helse.rapids_rivers.asLocalDate
 import no.nav.hjelpemidler.soknad.mottak.client.SøknadForRiverClient
 import no.nav.hjelpemidler.soknad.mottak.metrics.Prometheus
+import no.nav.hjelpemidler.soknad.mottak.service.Status
+import no.nav.hjelpemidler.soknad.mottak.service.VedtaksresultatLagretData
+import java.time.LocalDate
 import java.util.UUID
 
 private val logger = KotlinLogging.logger {}
@@ -24,11 +27,12 @@ internal class VedtaksresultatFraInfotrygd(
     init {
         River(rapidsConnection).apply {
             validate { it.demandValue("eventName", "hm-VedtaksResultatFraInfotrygd") }
-            validate { it.requireKey("søknadID", "vedtaksResultat", "vedtaksDato") }
+            validate { it.requireKey("søknadID", "fnrBruker", "vedtaksResultat", "vedtaksDato") }
         }.register(this)
     }
 
     private val JsonMessage.søknadID get() = this["søknadID"].textValue()
+    private val JsonMessage.fnrBruker get() = this["fnrBruker"].textValue()
     private val JsonMessage.vedtaksResultat get() = this["vedtaksResultat"].textValue()
     private val JsonMessage.vedtaksDato get() = this["vedtaksDato"].asLocalDate()
 
@@ -36,22 +40,53 @@ internal class VedtaksresultatFraInfotrygd(
         runBlocking {
             withContext(Dispatchers.IO) {
                 launch {
+
                     val søknadsId = UUID.fromString(packet.søknadID)
 
-                    kotlin.runCatching {
-                        søknadForRiverClient.lagreVedtaksresultat(søknadsId, packet.vedtaksResultat, packet.vedtaksDato)
-                    }.onSuccess {
-                        if (it == 0) {
-                            logger.warn("Ingenting ble endret når vi forsøkte å lagre vedtaksresultat for søknadsId=$søknadsId")
-                        } else {
-                            logger.info("Vedtaksresultat er nå lagra for søknadsId=$søknadsId")
-                            Prometheus.vedtaksresultatLagretCounter.inc()
-                        }
-                    }.onFailure {
-                        logger.error(it) { "Feil under lagring av vedtaksresultat for søknadsId=$søknadsId" }
-                    }.getOrThrow()
+                    lagreVedtaksresultat(søknadsId, packet.vedtaksResultat, packet.vedtaksDato)
+
+                    val status = when (packet.vedtaksResultat) {
+                        "I" -> Status.VEDTAKSRESULTAT_INNVILGET
+                        "IM" -> Status.VEDTAKSRESULTAT_MUNTLIG_INNVILGET
+                        "A" -> Status.VEDTAKSRESULTAT_AVSLÅTT
+                        "DI" -> Status.VEDTAKSRESULTAT_DELVIS_INNVILGET
+                        else -> Status.VEDTAKSRESULTAT_ANNET
+                    }
+                    oppdaterStatus(søknadsId, status)
+
+                    val vedtaksresultatLagretData =
+                        VedtaksresultatLagretData(søknadsId, packet.fnrBruker, packet.vedtaksResultat)
+                    context.send(vedtaksresultatLagretData.toJson("hm-VedtaksresultatLagret"))
                 }
             }
         }
     }
+
+    private suspend fun lagreVedtaksresultat(søknadsId: UUID, vedtaksresultat: String, vedtaksdato: LocalDate) {
+        kotlin.runCatching {
+            søknadForRiverClient.lagreVedtaksresultat(søknadsId, vedtaksresultat, vedtaksdato)
+        }.onSuccess {
+            if (it == 0) {
+                logger.warn("Ingenting ble endret når vi forsøkte å lagre vedtaksresultat for søknadsId=$søknadsId")
+            } else {
+                logger.info("Vedtaksresultat er nå lagra for søknadsId=$søknadsId vedtaksResultat=$vedtaksresultat vedtaksDato=$vedtaksdato")
+                Prometheus.vedtaksresultatLagretCounter.inc()
+            }
+        }.onFailure {
+            logger.error(it) { "Feil under lagring av vedtaksresultat for søknadsId=$søknadsId" }
+        }.getOrThrow()
+    }
+
+    private suspend fun oppdaterStatus(søknadsId: UUID, status: Status) =
+        kotlin.runCatching {
+            søknadForRiverClient.oppdaterStatus(søknadsId, status)
+        }.onSuccess {
+            if (it > 0) {
+                logger.info("Status på søknad sett til $status for søknadId $søknadsId, it=$it")
+            } else {
+                logger.warn("Status er allereie sett til $status for søknadId $søknadsId")
+            }
+        }.onFailure {
+            logger.error("Failed to update status to $status for søknadId $søknadsId")
+        }.getOrThrow()
 }
