8d4be5822fd84a3df7aac5b44936d9653379d689
erlingjd <16976922+erlingjd@users.noreply.github.com>
2021-09-28 11:09:35 +0200

    Midlertidig ignorer korrupte kafka-meldinger i dev
    Co-authored-by: kristoiv <kristoffer.andreas.iversen@nav.no>

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/river/PacketListenerWithOnError.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/river/PacketListenerWithOnError.kt
@@ -11,7 +11,8 @@ class RiverRequiredKeyMissingException(msg: String) : Exception(msg)
 
 interface PacketListenerWithOnError : River.PacketListener {
     override fun onError(problems: MessageProblems, context: MessageContext) {
-        sikkerlogg.info("River required keys had problems in parsing message from rapid: ${problems.toExtendedReport()}")
-        throw RiverRequiredKeyMissingException("River required keys had problems in parsing message from rapid, see Kibana index tjenestekall-* (sikkerlogg) for details")
+        return
+        // sikkerlogg.info("River required keys had problems in parsing message from rapid: ${problems.toExtendedReport()}")
+        // throw RiverRequiredKeyMissingException("River required keys had problems in parsing message from rapid, see Kibana index tjenestekall-* (sikkerlogg) for details")
     }
 }
