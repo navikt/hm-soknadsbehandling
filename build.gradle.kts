5abab71d4e9056985a86157d21854417c576f097
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-04-07 10:03:41 +0200

    Reodering deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,13 +46,13 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.0") // følger ikke kotlin-versjon
 
-    // Logging
+    // R&R and Logging fixes
+    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-09.40.11a466d7ac70")
     implementation("org.slf4j:slf4j-api:2.0.0-alpha6") // fordi rapids-and-rivers er på logback-classic:1.3.0-alpha10 som krever slf4j >= 2.0.0-alpha4
     implementation("io.github.microutils:kotlin-logging:2.1.21")
 
     // Other
     implementation("com.natpryce:konfig:1.6.10.0")
-    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-09.40.11a466d7ac70")
     implementation("com.github.guepardoapps:kulid:2.0.0.0")
     implementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 
