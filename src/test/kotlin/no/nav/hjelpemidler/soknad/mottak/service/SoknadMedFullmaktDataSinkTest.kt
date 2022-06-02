9c0e0d10986540d6ba2ea96d194f0af9d5230853
Gustav Lillesæter <gustav.lillesater@nav.no>
2022-06-03 00:45:55 +0200

    dokumentbeskrivelse for bestillinger

--- src/test/kotlin/no/nav/hjelpemidler/soknad/mottak/service/SoknadMedFullmaktDataSinkTest.kt
+++ src/test/kotlin/no/nav/hjelpemidler/soknad/mottak/service/SoknadMedFullmaktDataSinkTest.kt
@@ -37,7 +37,6 @@ internal class SoknadMedFullmaktDataSinkTest {
 
     @Test
     fun `Save soknad and mapping if packet contains required keys`() {
-
         val okPacket = """
             {
                 "eventName": "nySoknad",
@@ -47,6 +46,7 @@ internal class SoknadMedFullmaktDataSinkTest {
                 "fodselNrInnsender": "fodselNrInnsender",
                 "soknad": 
                     {
+                        "behovsmeldingType": "SØKNAD",
                         "soknad":
                             {
                                 "date": "2020-06-19",
@@ -67,6 +67,42 @@ internal class SoknadMedFullmaktDataSinkTest {
         capturedSoknadData.captured.fnrBruker shouldBe "fnrBruker"
         capturedSoknadData.captured.fnrInnsender shouldBe "fodselNrInnsender"
         capturedSoknadData.captured.status shouldBe Status.GODKJENT_MED_FULLMAKT
+        capturedSoknadData.captured.soknadGjelder shouldBe "Søknad om hjelpemidler"
+    }
+
+    @Test
+    fun `Save bestilling and mapping if packet contains required keys`() {
+        val okPacket = """
+            {
+                "eventName": "nySoknad",
+                "signatur": "FULLMAKT",
+                "eventId": "62f68547-11ae-418c-8ab7-4d2af985bcd8",
+                "fodselNrBruker": "fnrBruker",
+                "fodselNrInnsender": "fodselNrInnsender",
+                "soknad": 
+                    {
+                        "behovsmeldingType": "BESTILLING",
+                        "soknad":
+                            {
+                                "date": "2020-06-19",
+                                "bruker": 
+                                    {
+                                        "fornavn": "fornavn",
+                                        "etternavn": "etternavn"
+                                    },
+                                "id": "62f68547-11ae-418c-8ab7-4d2af985bcd8"
+                            }
+                    },
+                "kommunenavn": "Oslo"
+            }
+        """.trimMargin()
+
+        rapid.sendTestMessage(okPacket)
+
+        capturedSoknadData.captured.fnrBruker shouldBe "fnrBruker"
+        capturedSoknadData.captured.fnrInnsender shouldBe "fodselNrInnsender"
+        capturedSoknadData.captured.status shouldBe Status.GODKJENT_MED_FULLMAKT
+        capturedSoknadData.captured.soknadGjelder shouldBe "Bestilling av hjelpemidler"
     }
 
     @Test
@@ -220,6 +256,7 @@ internal class SoknadMedFullmaktDataSinkTest {
         jsonNode["eventName"].textValue() shouldBe "hm-søknadMedFullmaktMottatt"
         jsonNode["opprettet"].textValue() shouldNotBe null
         jsonNode["navnBruker"].textValue() shouldBe "etternavn fornavn"
+        jsonNode["soknadGjelder"].textValue() shouldBe "Søknad om hjelpemidler"
     }
 
     @Test
