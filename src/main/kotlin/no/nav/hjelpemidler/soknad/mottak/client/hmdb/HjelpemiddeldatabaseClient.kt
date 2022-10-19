1047b97306eea4ca4643327141bc2a27fbfb5f8f
Erik Eiesland <erik@eies.land>
2022-10-19 16:13:00 +0200

    bruker nytt felt i hm-grunndata-api for henting av produkter

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/client/hmdb/HjelpemiddeldatabaseClient.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/client/hmdb/HjelpemiddeldatabaseClient.kt
@@ -6,11 +6,11 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import mu.KotlinLogging
 import no.nav.hjelpemidler.soknad.mottak.Configuration
-import no.nav.hjelpemidler.soknad.mottak.client.hmdb.hentisokortnavnforproduktermedhmsnrs.Produkt
+import no.nav.hjelpemidler.soknad.mottak.client.hmdb.hentprodukter.Produkt
 import java.net.URL
 
 object HjelpemiddeldatabaseClient {
-    private val logg = KotlinLogging.logger {}
+    private val log = KotlinLogging.logger {}
     private val client =
         GraphQLKtorClient(
             url = URL("${Configuration.hmdb.grunndataApi}/graphql"),
@@ -18,27 +18,28 @@ object HjelpemiddeldatabaseClient {
             serializer = GraphQLClientJacksonSerializer()
         )
 
-    suspend fun hentIsokortnavnForProdukterMedHmsnrs(hmsnrs: Set<String>): List<Produkt> {
-        if (hmsnrs.isEmpty()) return emptyList()
-        logg.debug { "Henter isokortnavn for produkter med hmsnrs=$hmsnrs fra hjelpemiddeldatabasen" }
-        val request = HentIsokortnavnForProdukterMedHmsnrs(variables = HentIsokortnavnForProdukterMedHmsnrs.Variables(hmsnrs = hmsnrs.toList()))
+    suspend fun hentProdukter(hmsnr: Set<String>): List<Produkt> {
+        if (hmsnr.isEmpty()) return emptyList()
+        log.debug { "Henter produkter med hmsnr=$hmsnr fra hjelpemiddeldatabasen" }
+        val request = HentProdukter(variables = HentProdukter.Variables(hmsnr = hmsnr.toList()))
         return try {
             val response = client.execute(request)
             when {
                 response.errors != null -> {
-                    logg.error { "Feil under henting av data fra hjelpemiddeldatabasen, hmsnrs=$hmsnrs, errors=${response.errors?.map { it.message }}" }
-                    throw Exception("Feil under henting av data fra hjelpemiddeldatabasen, hmsnrs=$hmsnrs, errors=${response.errors?.map { it.message }}")
+                    log.error { "Feil under henting av data fra hjelpemiddeldatabasen, hmsnr=$hmsnr, errors=${response.errors?.map { it.message }}" }
+                    throw Exception("Feil under henting av data fra hjelpemiddeldatabasen, hmsnr=$hmsnr, errors=${response.errors?.map { it.message }}")
                 }
+
                 response.data != null -> {
                     val produkter = response.data?.produkter ?: emptyList()
-                    logg.debug { "Hentet ${produkter.size} isokortnavn for produkter fra hjelpemiddeldatabasen" }
+                    log.debug { "Hentet ${produkter.size} isokortnavn for produkter fra hjelpemiddeldatabasen" }
                     produkter
                 }
                 else -> throw Exception("Unexpected response: $response")
             }
         } catch (e: Exception) {
-            logg.error(e) { "Feil under henting av data fra hjelpemiddeldatabasen, hmsnrs=$hmsnrs" }
-            throw Exception("Feil under henting av data fra hjelpemiddeldatabasen, hmsnrs=$hmsnrs")
+            log.error(e) { "Feil under henting av data fra hjelpemiddeldatabasen, hmsnr=$hmsnr" }
+            throw Exception("Feil under henting av data fra hjelpemiddeldatabasen, hmsnr=$hmsnr")
         }
     }
 }
