fa401a12891a0daf2de6a21a16beaa36c110004d
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-01-26 08:55:20 +0100

    Ignorer kjente innsendinger ved varsel om innsendinger som har stoppet opp

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/service/MonitoreringService.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/service/MonitoreringService.kt
@@ -36,15 +36,25 @@ internal class MonitoreringService(
         )
     }
 
+    /*
+    Dette er saker som har stoppet opp pga manglende verdier i innsendingen.
+    Feilen skal ha blitt rettet, og formidler har blitt informert om at de må sende inn på nytt.
+    Det må vurderes konsekvens av å slette disse fra systemet dersom vi evt. skal gjøre det.
+    F.eks.: Er sakene synlig for bruker?
+     */
+    private val ignoreList = listOf("bdd3b385-9add-4317-bf82-d8df0e6974e0", "ae15f3a2-f74b-4465-a636-7f527c98d0a0")
+
     internal suspend fun rapporterGodkjenteSøknaderUtenOppgave() {
         logger.info { "Starter sjekk på om det finnes godkjente søknader som mangler oppgave" }
         try {
             val godkjenteSøknaderUtenOppgave = søknadForRiverClient.hentGodkjenteSøknaderUtenOppgaveEldreEnn(TO_DAGER)
+                .filter { it !in ignoreList }
+
             if (godkjenteSøknaderUtenOppgave.isNotEmpty()) {
                 logger.error {
                     "Det finnes ${godkjenteSøknaderUtenOppgave.size} godkjente søknader uten oppgave som er eldre enn $TO_DAGER dager. " +
-                        "Undersøk hvorfor disse har stoppet opp i systemet. " +
-                        "søknads-IDer (max 10): ${godkjenteSøknaderUtenOppgave.take(10).joinToString()}"
+                            "Undersøk hvorfor disse har stoppet opp i systemet. " +
+                            "søknads-IDer (max 10): ${godkjenteSøknaderUtenOppgave.take(10).joinToString()}"
                 }
             }
             // Trigger alerts til Slack ved å følge med på denne Prometheus-metrikken
