9d1c0febeefd10180fb5335237c031482e198952
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2024-02-28 13:44:53 +0100

    øk timeout til 2 min

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/client/OebsClient.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/client/OebsClient.kt
@@ -1,25 +1,44 @@
 package no.nav.hjelpemidler.soknad.mottak.client
 
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.header
 import io.ktor.client.request.request
-import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpMethod
+import io.ktor.serialization.jackson.jackson
 import mu.KotlinLogging
 import no.nav.hjelpemidler.soknad.mottak.aad.AzureClient
-import no.nav.hjelpemidler.soknad.mottak.httpClient
 
 private val logg = KotlinLogging.logger {}
 private val secureLogg = KotlinLogging.logger("tjenestekall")
+
+private fun oebsHttpClient(): HttpClient = HttpClient(Apache) {
+    expectSuccess = true
+    install(ContentNegotiation) {
+        jackson {
+            registerModule(JavaTimeModule())
+            disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        }
+    }
+    install(HttpTimeout) {
+        requestTimeoutMillis = 2*60*1000
+    }
+}
 
 class OebsClient(
     private val azureClient: AzureClient,
     private val baseUrl: String,
     private val apiScope: String,
-    private val httpClient: HttpClient = httpClient()
+    private val httpClient: HttpClient = oebsHttpClient()
 ) {
     suspend fun hentBrukerpassrollebytter(): List<Brukerpassrollebytter> {
         try {
