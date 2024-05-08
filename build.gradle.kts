eab1ab5e1b269e496e17f05ef109caff0f727390
Erik Eiesland <erik.eiesland@nav.no>
2024-05-08 10:37:45 +0200

    bruker version catalog fra hm-katalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,86 +1,69 @@
-
 import com.expediagroup.graphql.plugin.gradle.tasks.GraphQLIntrospectSchemaTask
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
-val graphQLClientVersion = "6.5.6"
-
 plugins {
-    kotlin("jvm") version "1.9.23"
-    id("com.expediagroup.graphql") version "6.5.6"
-    id("io.ktor.plugin") version "2.3.10"
-    id("com.diffplug.spotless") version "6.25.0"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.graphql)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.spotless)
 }
 
 group = "no.nav.hjelpemidler.soknad.mottak"
-
-repositories {
-    mavenCentral()
-    maven("https://packages.confluent.io/maven/")
-    maven("https://jitpack.io")
-}
 
 application {
     applicationName = "hm-soknadsbehandling"
     mainClass.set("no.nav.hjelpemidler.soknad.mottak.ApplicationKt")
 }
 
-fun ktor(name: String) = "io.ktor:ktor-$name"
-fun graphqlKotlin(name: String) = "com.expediagroup:graphql-kotlin-$name:$graphQLClientVersion"
-
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:2024020419561707073004.70bfb92c077c")
+    // Kotlin
+    implementation(libs.kotlin.stdlib)
+
+    // Rapids and Rivers
+    implementation(libs.rapidsAndRivers)
 
     // Logging
-    runtimeOnly("ch.qos.logback:logback-classic:1.5.5")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
-    implementation("io.github.microutils:kotlin-logging:3.0.5")
-
-
-    // Kotlin
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3") // følger ikke kotlin-versjon
+    implementation(libs.kotlin.logging.deprecated)
+    runtimeOnly(libs.logback.classic)
+    runtimeOnly(libs.logstash.logback.encoder)
 
     // Other
-    implementation("com.natpryce:konfig:1.6.10.0")
+    // NB! Bytt til løsning fra hm-http
+    implementation(libs.konfig.deprecated)
+    // NB! Dette biblioteket vedlikeholdes ikke, hadde det holdt med UUID?
     implementation("com.github.guepardoapps:kulid:2.0.0.0")
 
     // Jackson
-    val jacksonVersion = "2.17.0"
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    implementation(libs.jackson.databind)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.jackson.datatype.jsr310)
 
-    // Ktor Client
-    implementation(ktor("client-core"))
-    implementation(ktor("client-apache"))
-    implementation(ktor("serialization-jackson"))
-    implementation(ktor("client-content-negotiation"))
-    implementation(ktor("client-jackson"))
+    // Ktor
+    implementation(libs.ktor.serialization.jackson)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.apache)
+    implementation(libs.ktor.client.content.negotiation)
 
     // InfluxDB
-    implementation("org.influxdb:influxdb-java:2.24")
-    implementation("com.influxdb:influxdb-client-kotlin:7.0.0")
+    implementation(libs.influxdb.client.kotlin)
 
     // GraphQL Client
-    implementation(graphqlKotlin("ktor-client")) {
+    implementation(libs.graphql.ktor.client) {
         exclude("com.expediagroup", "graphql-kotlin-client-serialization") // prefer jackson
         exclude("io.ktor", "ktor-client-serialization") // prefer ktor-client-jackson
         exclude("io.ktor", "ktor-client-cio") // prefer ktor-client-apache
     }
-    implementation(graphqlKotlin("client-jackson"))
+    implementation(libs.graphql.client.jackson)
 
     // Test
-    testImplementation(kotlin("test"))
-    testImplementation(ktor("server-test-host"))
-    testImplementation("io.mockk:mockk:1.13.10")
-    val kotestVersion = "5.8.1"
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotest.assertions.core)
 }
 
-kotlin { jvmToolchain { languageVersion.set(JavaLanguageVersion.of(21)) } }
+kotlin { jvmToolchain(21) }
 
 tasks {
     test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,24 @@
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        maven("https://jitpack.io")
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/navikt/*")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+        maven {
+            url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+        }
+    }
+    versionCatalogs {
+        create("libs") {
+            from("no.nav.hjelpemidler:hm-katalog:0.1.43")
+        }
+    }
+}
+
 rootProject.name = "hm-soknadsbehandling"
