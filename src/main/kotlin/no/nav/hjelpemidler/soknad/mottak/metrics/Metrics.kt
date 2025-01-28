3f386cf2daf78fb084d3b983c29da7eebacf4172
Erik Eiesland <erik.eiesland@nav.no>
2025-01-28 10:12:18 +0100

    logger tilfeller hvor vi ikke får utledet dokumenttittel

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/metrics/Metrics.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/metrics/Metrics.kt
@@ -1,19 +1,11 @@
 package no.nav.hjelpemidler.soknad.mottak.metrics
 
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
-import com.influxdb.client.InfluxDBClientFactory
-import com.influxdb.client.domain.WritePrecision
-import com.influxdb.client.write.Point
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import no.nav.hjelpemidler.configuration.InfluxDBEnvironmentVariable
-import no.nav.hjelpemidler.configuration.NaisEnvironmentVariable
+import no.nav.hjelpemidler.metrics.createMetricsProducer
 import no.nav.hjelpemidler.soknad.mottak.client.PdlClient
 import no.nav.hjelpemidler.soknad.mottak.metrics.kommune.KommuneDto
 import no.nav.hjelpemidler.soknad.mottak.metrics.kommune.KommuneService
-import java.time.Instant
 import java.util.UUID
 
 class Metrics(
@@ -21,46 +13,34 @@ class Metrics(
     private val pdlClient: PdlClient,
     private val kommuneService: KommuneService = KommuneService(),
 ) {
-
-    private val client = InfluxDBClientFactory.createV1(
-        "${InfluxDBEnvironmentVariable.INFLUX_HOST}:${InfluxDBEnvironmentVariable.INFLUX_PORT}",
-        InfluxDBEnvironmentVariable.INFLUX_USER,
-        InfluxDBEnvironmentVariable.INFLUX_PASSWORD.toCharArray(),
-        InfluxDBEnvironmentVariable.INFLUX_DATABASE_NAME,
-        null
-    )
-    private val writeApi = client.makeWriteApi()
-    private val metricsProducer = MetricsProducer(messageContext)
+    private val metricsProducer = createMetricsProducer()
+    private val kafkaMetricsProducer = MetricsProducer(messageContext)
 
     suspend fun digitalSøknad(brukersFnr: String, soknadId: UUID) {
-        withContext(Dispatchers.IO) {
-            try {
-                val kommunenr = pdlClient.hentKommunenr(brukersFnr)
-                val sted = kommunenrTilSted(kommunenr)
-                writeEvent(
-                    STED,
-                    mapOf("counter-digital" to 1L),
-                    mapOf("kommune" to sted.kommunenavn, "fylke" to sted.fylkenavn)
-                )
-            } catch (e: Exception) {
-                log.warn(e) { "Feil under logging av statistikk 'digital søknad per kommune', søknadId: $soknadId" }
-            }
+        try {
+            val kommunenr = pdlClient.hentKommunenr(brukersFnr)
+            val sted = kommunenrTilSted(kommunenr)
+            writeEvent(
+                STED,
+                mapOf("counter-digital" to 1L),
+                mapOf("kommune" to sted.kommunenavn, "fylke" to sted.fylkenavn)
+            )
+        } catch (e: Exception) {
+            log.warn(e) { "Feil under logging av statistikk 'digital søknad per kommune', søknadId: $soknadId" }
         }
     }
 
     suspend fun papirsøknad(brukersFnr: String) {
-        withContext(Dispatchers.IO) {
-            try {
-                val kommunenr = pdlClient.hentKommunenr(brukersFnr)
-                val sted = kommunenrTilSted(kommunenr)
-                writeEvent(
-                    STED,
-                    mapOf("counter-papir" to 1L),
-                    mapOf("kommune" to sted.kommunenavn, "fylke" to sted.fylkenavn)
-                )
-            } catch (e: Exception) {
-                log.warn(e) { "Feil under logging av statistikk 'papirsøknad per kommune'" }
-            }
+        try {
+            val kommunenr = pdlClient.hentKommunenr(brukersFnr)
+            val sted = kommunenrTilSted(kommunenr)
+            writeEvent(
+                STED,
+                mapOf("counter-papir" to 1L),
+                mapOf("kommune" to sted.kommunenavn, "fylke" to sted.fylkenavn)
+            )
+        } catch (e: Exception) {
+            log.warn(e) { "Feil under logging av statistikk 'papirsøknad per kommune'" }
         }
     }
 
@@ -69,22 +49,20 @@ class Metrics(
         vedtaksresultat: String,
         soknadsType: String,
     ) {
-        withContext(Dispatchers.IO) {
-            try {
-                val kommunenr = pdlClient.hentKommunenr(brukersFnr)
-                val sted = kommunenrTilSted(kommunenr)
-                writeEvent(
-                    "$VEDTAKSRESULTAT_INFOTRYGD.$soknadsType",
-                    mapOf("counter" to 1L),
-                    mapOf(
-                        "vedtaksresultat" to vedtaksresultat,
-                        "kommune" to sted.kommunenavn,
-                        "fylke" to sted.fylkenavn
-                    )
+        try {
+            val kommunenr = pdlClient.hentKommunenr(brukersFnr)
+            val sted = kommunenrTilSted(kommunenr)
+            writeEvent(
+                "$VEDTAKSRESULTAT_INFOTRYGD.$soknadsType",
+                mapOf("counter" to 1L),
+                mapOf(
+                    "vedtaksresultat" to vedtaksresultat,
+                    "kommune" to sted.kommunenavn,
+                    "fylke" to sted.fylkenavn
                 )
-            } catch (e: Exception) {
-                log.warn(e) { "Feil under logging av statistikk 'resultat fra infotrygd'." }
-            }
+            )
+        } catch (e: Exception) {
+            log.warn(e) { "Feil under logging av statistikk 'resultat fra infotrygd'." }
         }
     }
 
@@ -98,35 +76,18 @@ class Metrics(
         }
     }
 
-    private fun writeEvent(measurement: String, fields: Map<String, Any>, tags: Map<String, String>) =
-        runBlocking(Dispatchers.IO) {
-            try {
-                val point = Point(measurement)
-                    .addTags(DEFAULT_TAGS)
-                    .addTags(tags)
-                    .addFields(fields)
-                    .time(Instant.now().toEpochMilli(), WritePrecision.MS)
-
-                log.debug {
-                    "Sender metrikk: ${point.toLineProtocol()}"
-                }
-                writeApi.writePoint(point)
-                metricsProducer.hendelseOpprettet(measurement, fields, tags)
-            } catch (e: Exception) {
-                log.warn(e) { "Sending av metrikk feilet" }
-            }
+    private suspend fun writeEvent(measurement: String, fields: Map<String, Any>, tags: Map<String, String>) =
+        try {
+            metricsProducer.writeEvent(measurement, fields, tags)
+            kafkaMetricsProducer.hendelseOpprettet(measurement, fields, tags)
+        } catch (e: Exception) {
+            log.warn(e) { "Publisering av metrikk feilet" }
         }
 }
 
+private val log = KotlinLogging.logger {}
 private val ukjentSted = KommuneDto("UKJENT", "UKJENT", "UKJENT")
 
-private val log = KotlinLogging.logger {}
-
-private val DEFAULT_TAGS: Map<String, String> = mapOf(
-    "application" to NaisEnvironmentVariable.NAIS_APP_NAME,
-    "cluster" to NaisEnvironmentVariable.NAIS_CLUSTER_NAME,
-)
-
 private const val PREFIX = "hm-soknadsbehandling"
-const val STED = "$PREFIX.sted"
-const val VEDTAKSRESULTAT_INFOTRYGD = "$PREFIX.vedtaksresultat-infotrygd"
+private const val STED = "$PREFIX.sted"
+private const val VEDTAKSRESULTAT_INFOTRYGD = "$PREFIX.vedtaksresultat-infotrygd"
