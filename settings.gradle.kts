80ab873eddcda3e89f36de8693fc01495454c55f
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-06-04 11:46:36 +0200

    Hotlibs 25.155.103050 (IKKE_INNHENTET_FORDI_KUN_TILBEHØR_V3)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:katalog:25.148.105431")
+            from("no.nav.hjelpemidler:katalog:25.155.103050")
         }
     }
 }
