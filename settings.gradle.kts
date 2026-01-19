2f76fac6c0253ebe08b46cea373a8266616b949a
murvold <mads.severin.murvold@nav.no>
2026-01-19 10:28:46 +0100

    Fjerner gammel slacklogging og Hotlibs -> 26.018.135750

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:katalog:25.211.083105")
+            from("no.nav.hjelpemidler:katalog:26.018.135750")
         }
     }
 }
