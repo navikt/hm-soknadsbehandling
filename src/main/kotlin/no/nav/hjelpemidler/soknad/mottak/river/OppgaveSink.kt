80ddb13e53fb49e0336cd8a7b7561eff01b80bc6
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2026-02-05 14:18:46 +0100

    Send event for digital søknad oppgaveid

--- src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/river/OppgaveSink.kt
+++ src/main/kotlin/no/nav/hjelpemidler/soknad/mottak/river/OppgaveSink.kt
@@ -5,7 +5,10 @@ import com.github.navikt.tbd_libs.rapids_and_rivers.River
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import io.github.oshai.kotlinlogging.KotlinLogging
+import no.nav.hjelpemidler.behovsmeldingsmodell.TilknyttetSøknad
 import no.nav.hjelpemidler.soknad.mottak.client.SøknadsbehandlingClient
+import no.nav.hjelpemidler.soknad.mottak.melding.Melding
+import java.util.UUID
 
 private val log = KotlinLogging.logger {}
 
@@ -25,6 +28,7 @@ class OppgaveSink(
         }.register(this)
     }
 
+    private val JsonMessage.eventName get() = this["eventName"].textValue()
     private val JsonMessage.søknadId get() = uuidValue("soknadId")
     private val JsonMessage.oppgaveId get() = this["oppgaveId"].textValue()
 
@@ -44,5 +48,30 @@ class OppgaveSink(
             log.error(e) { "Håndtering av ny oppgaveId: $oppgaveId for søknadId: $søknadId feilet" }
             throw e
         }
+
+        try {
+            if (søknadsbehandlingClient.finnSøknad(søknadId)?.digital == true) {
+                log.info { "Sender oppgave for digital søknad opprettet til infotrygd-poller for søknadId: $søknadId, oppgaveId: $oppgaveId" }
+                context.publish(
+                    søknadId.toString(),
+                    DigitalSøknadOppgaveIdMelding(
+                        søknadId = søknadId,
+                        oppgaveId = oppgaveId,
+                        tilbakeført = packet.eventName == "hm-opprettetJournalføringsoppgaveForTilbakeførtSak",
+                    )
+                )
+            }
+        } catch (e: Exception) {
+            log.error(e) { "Feilet i å sjekke om søknad er digital eller å sende melding om digital oppgaveid" }
+        }
     }
+}
+
+data class DigitalSøknadOppgaveIdMelding(
+    override val søknadId: UUID,
+    val oppgaveId: String,
+    val tilbakeført: Boolean,
+) : TilknyttetSøknad, Melding {
+    override val eventId: UUID = UUID.randomUUID()
+    override val eventName: String = "hm-digital-søknad-oppgaveId"
 }
